2a4969e3c0821af90275b7b06166a556e5a970d3
Yehor Lohutov <39864474+LogutovEgor@users.noreply.github.com>
2021-10-12 22:21:14 +0300

    Created console application.

---
+++ FuzzyAdditiveRatioAssessment/FuzzyAdditiveRatioAssessment/LinguisticTerms.cs
@@ -0,0 +1,23 @@
+﻿namespace FuzzyAdditiveRatioAssessment
+{
+    public static class LinguisticTerms
+    {
+        public const string VERY_LOW = "VL"; // very low 
+        public const string LOW = "L"; // low 
+        public const string MEDIUM_LOW = "ML"; // medium low
+        public const string MEDIUM = "M"; // medium
+        public const string MEDIUM_HIGH = "MH"; // medium high
+        public const string HIGH = "H"; // high
+        public const string VERY_HIGH = "VH"; // very high
+
+        public static string[] GetAll => new string[7] {
+            VERY_LOW,
+            LOW,
+            MEDIUM_LOW,
+            MEDIUM,
+            MEDIUM_HIGH,
+            HIGH,
+            VERY_HIGH
+        };
+    }
+}
---
+++ FuzzyAdditiveRatioAssessment/FuzzyAdditiveRatioAssessment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using FuzzyAdditiveRatioAssessment;
+
+Console.WriteLine("Enter the number of criteria.");
+int criterias = ConsoleInput.ReadIntAboveOrEquals(1);
+
+Console.WriteLine("Enter the number of alternatives.");
+int alternatives = ConsoleInput.ReadIntAboveOrEquals(2);
+
+Console.WriteLine("Enter the number of experts.");
+int experts = ConsoleInput.ReadIntAboveOrEquals(1);
+
+Console.WriteLine("Enter the importance of criteria.");
+string[,] criteriasLinguisticImportance = new string[experts, criterias];
+for (int expert = 0; expert < experts; expert++)
+    for (int criteria = 0; criteria < criterias; criteria++)
+    {
+        Console.WriteLine($"Expert {expert + 1}, criteria {criteria + 1}:");
+        criteriasLinguisticImportance[expert, criteria] = ConsoleInput.ChooseFrom(LinguisticTerms.GetAll);
+    }
+
+
+Console.WriteLine("Enter estimates of alternatives.");
+string[,,] alternativesEstimates = new string[experts, alternatives, criterias];
+for (int expert = 0; expert < experts; expert++)
+    for (int alternative = 0; alternative < alternatives; alternative++)
+        for (int criteria = 0; criteria < criterias; criteria++)
+        {
+            Console.WriteLine($"Expert {expert + 1}, alternative {alternative + 1}, criteria {criteria + 1}:");
+            alternativesEstimates[expert, alternative, criteria] = ConsoleInput.ChooseFrom(LinguisticTerms.GetAll);
+        }
+
+
+
+Console.WriteLine("asdas");
+//namespace FuzzyAdditiveRatioAssessment
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello World!");
+//        }
+//    }
+//}
